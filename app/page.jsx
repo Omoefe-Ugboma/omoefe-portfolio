5ddb260a2e82d8b421de2f89b33c35e258912fdf
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2025-05-29 12:01:36 +0400

    Update on my new portfolio

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+"use client";
+import Blob from '@/components/Blob';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+  return (
+    <motion.section
+     initial={{opacity: 0}}
+     animate={{
+      opacity: 1,
+      transition: { delay: 2.4, duration: 0.4, ease: "easeIn"},
+     }}
+     className="h-screen flex items-center"
+     >
+      <div className='flex flex-col xl:flex-row items-center justify-between w-full'>
+         <div className='w-full xl:w-[550px]'>Text</div>
+         <div className='hidden xl:block flex-1 relative z-20'>
+          <Blob/>
+         </div>
+      </div>
+    </motion.section>
+  )
+}
+
+export default Home;
---
+++ components/PageTransition.jsx
@@ -0,0 +1,26 @@
+// import React, { Children } from 'react'
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+const PageTransition = ({ children}) => {
+    const pathname = usePathname();
+  return (
+     <AnimatePresence>
+        <div key={pathname}>
+            <motion.dev
+             initial={{ opacity: 1}}
+             animate={{
+                opacity: 0,
+                transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+             }}
+             className="min-h-screen w-screen fixed top-0 pointer-events-none"
+            />
+            {children}
+        </div>
+     </AnimatePresence>
+  )
+}
+
+export default PageTransition
